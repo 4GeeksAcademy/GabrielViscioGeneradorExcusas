73e07f36e5ea8c5028d595069e9a8051470dac11
Gabrielviscio10 <gonzalo.viscio@icloud.com>
2024-06-12 20:39:50 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const who = ["The dog", "My grandma", "The mailman", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "my phone", "the car"];
+  const when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const randomWho = Aleatorias(who);
+  const randomAction = Aleatorias(action);
+  const randomWhat = Aleatorias(what);
+  const randomWhen = Aleatorias(when);
+
+  const randomSentence = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+
+  const resultado = document.getElementById("texto");
+  resultado.innerHTML = randomSentence;
 };
+
+function Aleatorias(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+console.log(Aleatorias);
